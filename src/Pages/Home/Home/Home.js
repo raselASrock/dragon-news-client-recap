3c0bf5b3301ff27b3369e104391bb0b598d05898
raSel <mdraselm413@gmail.com>
2022-10-21 23:00:32 +0600

    card

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSammaryCard from '../../Shared/NewsSammaryCard/NewsSammaryCard';
 
 const Home = () => {
+    const allNews = useLoaderData()
     return (
         <div>
-            <h2>This is Home Component.</h2>
+            <h2>Dragon news Home: {allNews.length}</h2>
+            {
+                allNews.map(news => <NewsSammaryCard
+                key={news._id}
+                news ={news}
+                ></NewsSammaryCard>)
+            }
         </div>
     );
 };
---
+++ src/Pages/Shared/NewsSammaryCard/NewsSammaryCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsSammaryCard = ({news}) => {
+    return (
+        <div>
+            {news.title}
+        </div>
+    );
+};
+
+export default NewsSammaryCard;
